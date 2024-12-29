6db06d14341b75ecfaa2f44c5665ca6beada56b2
Ryan Whitney <ryan.whitney@me.com>
2024-12-29 13:21:42 -0800

    Initial Commit

---
+++ VolumeControl/VolumeControlApp.swift
@@ -0,0 +1,32 @@
+//
+//  VolumeControlApp.swift
+//  VolumeControl
+//
+//  Created by Ryan Whitney on 12/29/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VolumeControlApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
